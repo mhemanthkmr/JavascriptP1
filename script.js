61548372830c5812c7985ef2c2c29f0ed439ca23
mhemanthkmr <m.hemanthkmr143@gmail.com>
2021-04-22 19:45:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function ageindays(){
+    var birthYear = prompt("What Year you Born .")
+    var ageinday = (2021 - birthYear) * 365
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageinday + ' Days Old.')
+    h1.setAttribute('id','ageindays')
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+function remo(){
+    document.getElementById('ageindays').remove()
+}
+
+//Generating a Cat
+
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
