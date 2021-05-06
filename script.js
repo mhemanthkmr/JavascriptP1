b13e7ed886e1a760f7bf51ebce392bd1aa4142ca
mhemanthkmr <m.hemanthkmr143@gmail.com>
2021-05-06 16:13:54 +0530

    create final messages

--- script.js
+++ script.js
@@ -26,8 +26,11 @@ function rpsGame(yourChoice){
     humanChoice = yourChoice.id ;
     botChoice = numberToChoice(randToRpsInt());
     //alert(botChoice);
-    // result = decideWinner(humanChoice,botChoice); // [0,1] Human Lost , Bot Won 
-    //message = finalMessagae(results); // "{ message : "You Won " , 'colr' : green )
+    console.log('Computer Choice',botChoice)
+    result = decideWinner(humanChoice,botChoice); // [0,1] Human Lost , Bot Won 
+    console.log(result);
+    message = finalMessage(result); // "{ message : "You Won " , 'colr' : green )
+    console.log(message);
     //rpsFrontEnd(yourChoice,id,botChoice,message);
 }
 
@@ -39,10 +42,27 @@ function numberToChoice(number){
     return ['rock','paper','scissors'][number] ;
 }
 
-function decideWinner(yourChoice , botChoice){
+function decideWinner(yourChoice , computerChoice){
     var rpsDatabase = {
         'rock' : {'scissors' : 1 ,'rock' : 0.5 ,'paper':0 },
         'paper' : {'rock' : 1 ,'paper' : 0.5 ,'scissors':0 },
         'scissors' : {'paper' : 1 ,'scissors' : 0.5 ,'rock':0 },
+    }
+
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return [yourScore,computerScore];
+}
+
+function finalMessage([yourScore,computerScore]){
+    if (yourScore === 0) {
+        return {'message' : 'You Lost !' ,'color' : 'red'};
+    }
+    else if ( yourScore === 0.5) {
+        return {'message' : 'You Tied !' , 'color' : 'yellow'}
+    }
+    else {
+        return {'message' : 'You Won !' , 'color' : 'green'}
     }
 }
