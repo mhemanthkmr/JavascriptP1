4be2c8c25dd5cd119d2593007dad46ac57c985ca
mhemanthkmr <m.hemanthkmr143@gmail.com>
2021-05-06 15:45:45 +0530

    added and created a functions like rand and number to choice

--- script.js
+++ script.js
@@ -20,6 +20,29 @@ function generateCat(){
     div.appendChild(image);
 }
 
-function rps(body){
-    
+function rpsGame(yourChoice){
+    //console.log(yourChoice);
+    var humanChoice , botChoice;
+    humanChoice = yourChoice.id ;
+    botChoice = numberToChoice(randToRpsInt());
+    //alert(botChoice);
+    // result = decideWinner(humanChoice,botChoice); // [0,1] Human Lost , Bot Won 
+    //message = finalMessagae(results); // "{ message : "You Won " , 'colr' : green )
+    //rpsFrontEnd(yourChoice,id,botChoice,message);
+}
+
+function randToRpsInt() {
+    return Math.floor(Math.random() * 3 );
+}
+
+function numberToChoice(number){
+    return ['rock','paper','scissors'][number] ;
+}
+
+function decideWinner(yourChoice , botChoice){
+    var rpsDatabase = {
+        'rock' : {'scissors' : 1 ,'rock' : 0.5 ,'paper':0 },
+        'paper' : {'rock' : 1 ,'paper' : 0.5 ,'scissors':0 },
+        'scissors' : {'paper' : 1 ,'scissors' : 0.5 ,'rock':0 },
+    }
 }
